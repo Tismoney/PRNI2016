ab79ddb4451782c1689c1a7470815bed9182854f
Tismoney <tismoney@mail.ru>
2017-08-12 21:41:30 +0300

    Init seed

--- Subsection/reskit/core.py
+++ Subsection/reskit/core.py
@@ -7,12 +7,13 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics.scorer import check_scoring
 from sklearn.pipeline import Pipeline
 from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.model_selection import PredefinedSplit
 
 from collections import OrderedDict
 from itertools import product
 from pandas import DataFrame
 from pickle import dump, load
-from numpy import mean, std, hstack, zeros
+from numpy import mean, std, hstack, zeros, loadtxt
 from time import time
 
 import os
@@ -138,8 +139,9 @@ class Pipeliner(object):
 
         self.plan_table = DataFrame().from_dict(plan_rows)[columns]
         self.named_steps = steps
-        self.eval_cv = eval_cv
-        self.grid_cv = grid_cv
+        self.test_fold = loadtxt('cv.txt')
+        self.eval_cv = PredefinedSplit(test_fold = self.test_fold[0])
+        self.grid_cv = PredefinedSplit(test_fold = self.test_fold[1])
         self.param_grid = param_grid
         self._cached_data = OrderedDict()
         self.best_params = dict()
@@ -457,11 +459,12 @@ class Pipeliner(object):
                                      cv=self.eval_cv,
                                      n_jobs=-1)
         else:
-            init_random_state = self.eval_cv.random_state
+            #init_random_state = self.eval_cv.random_state
             scores = list()
             for i in range(collect_n):
+                eval_cv = PredefinedSplit(test_fold = self.test_fold[1+i])
                 fold_prediction = cross_val_predict(Pipeline(steps), X, y,
-                                                    cv=self.eval_cv,
+                                                    cv=eval_cv,
                                                     n_jobs=-1)
                 metric = check_scoring(steps[-1][1],
                                        scoring=scoring).__dict__['_score_func']
@@ -472,9 +475,9 @@ class Pipeliner(object):
                 #                         n_jobs=-1)
                 #scores.append(mean(score_n))
                 
-                self.eval_cv.random_state += 1
+                #self.eval_cv.random_state += 1
 
-            self.eval_cv.random_state = init_random_state
+            #self.eval_cv.random_state = init_random_state
         return scores
 
 
